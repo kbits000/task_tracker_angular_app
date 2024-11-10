3677e9c581dd191064e0a02a7666e3be5de9f72b
kbits000 <69712340+kbits000@users.noreply.github.com>
2024-11-10 07:29:31 +0300

    initial

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { NgStyle } from '@angular/common';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [NgStyle],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+  @Input() text: string = '';
+  @Input() color: string = '';
+  @Output() btnClick = new EventEmitter();
+
+  onClick() {
+    console.log('Add');
+    this.btnClick.emit();
+  }
+}
